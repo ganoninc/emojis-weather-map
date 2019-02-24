4b5f329129cd7f1d12f545e12d517c5d3ba3b262
Romain Giovanetti <ganoninc@gmail.com>
2019-02-23 21:29:39 -0500

    add loading animation

--- src/EmojiWeatherMapEmoji.js
+++ src/EmojiWeatherMapEmoji.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import LoadingImage from "./EmojiWeatherMapEmojiLoading.svg";
 
 class EmojiWeatherMapEmoji extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: false
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+
+  }
   render() {
-    return (
-      <span className="emoji" role="img" aria-label="Sun">
-      ☀️
+    const { isLoading } = this.state;
+
+    if (isLoading){
+      return (
+        <span className="emoji" role="img" aria-label="Loading">
+          <img className="emoji-loading" src={LoadingImage} />
       </span>
-    );
+      );
+    } else {
+      return (
+        <span className="emoji" role="img" aria-label="Sun">
+          ☀️
+      </span>
+      );
+    }
   }
 }
 
