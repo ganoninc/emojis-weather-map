12f890843f6217003c0867c0b179ccb6285f35cc
Romain Giovanetti <ganoninc@gmail.com>
2019-04-04 21:06:02 -0400

    fetch data from ewm-data-provider

--- src/EmojiWeatherMapEmoji.js
+++ src/EmojiWeatherMapEmoji.js
@@ -6,28 +6,34 @@ class EmojiWeatherMapEmoji extends Component {
     super(props);
 
     this.state = {
-      isLoading: false
+      isLoading: false,
+      currentWeatherIcon: null
     };
+
+    this.dataProviderEndpoint = 'https://giovanetti.fr/ewm-data-provider/?latitude=' + props.geographicCoordinates.latitude + '&longitude=' + props.geographicCoordinates.longitude;
   }
 
   componentDidMount() {
     this.setState({ isLoading: true });
 
+    fetch(this.dataProviderEndpoint)
+      .then(response => response.text())
+      .then(data => this.setState({ isLoading: false, currentWeatherIcon: data }));
   }
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, currentWeatherIcon } = this.state;
 
     if (isLoading){
       return (
         <span className="emoji" role="img" aria-label="Loading">
-          <img className="emoji-loading" src={LoadingImage} />
-      </span>
+          <img className="emoji-loading" alt="emoji" src={LoadingImage} />
+        </span>
       );
     } else {
       return (
         <span className="emoji" role="img" aria-label="Sun">
-          ☀️
-      </span>
+          {currentWeatherIcon}
+        </span>
       );
     }
   }
