2bf52c734057584f31c6559ba0bae8ce16d51d4d
Romain Giovanetti <ganoninc@gmail.com>
2025-07-03 16:36:39 +0200

    add missing key prop to a group of span in EmojiWeatherMapOffset

--- src/EmojiWeatherMapOffset.js
+++ src/EmojiWeatherMapOffset.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class EmojiWeatherMapOffset extends Component {
-  
   render() {
-    let spaces = []; 
-    for (let index = 0; index <  this.props.length; index++) {
-      spaces.push(<span className="emojiWeatherMap__offsetSpace">&nbsp;</span>);
+    let spaces = [];
+
+    for (let index = 0; index < this.props.length; index++) {
+      spaces.push(
+        <span key={index} className="emojiWeatherMap__offsetSpace">
+          &nbsp;
+        </span>
+      );
     }
 
-    return (
-      spaces
-    );
+    return spaces;
   }
 }
 
